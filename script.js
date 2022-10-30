a469950efff5970cf0c38b599490caee6d0f5e63
malgos208 <87128328+sherlocked208@users.noreply.github.com>
2022-10-30 19:47:42 +0100

    fixed registration

--- script.js
+++ script.js
@@ -4,37 +4,49 @@
 //     window.location.reload();
 // }
 
-const forgotPass = document.getElementById("forgotPassword");
-forgotPass.on('click',forgotPassMessage);
+
 function forgotPassMessage(){
     r = document.getElementById("result");
-    r.innerHTML = "Wprowadź poświadczenia przekazane przez opiekuna (nazwa użytkownika z domeny tm1.edu.pl)."
+    r.innerHTML = "Wprowadź poświadczenia przekazane przez opiekuna (adres email z domeny tm1.edu.pl)";
 }
 
 
 btnNext = document.querySelector("#loginbtn");
 
 function validateEmail(e) {
-    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e);
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e) 
+    && e.indexOf("@tm1.edu.pl", e.length - "@tm1.edu.pl".length) !== -1;
 }
 function validate() {
     const r = document.getElementById("result"),
         u = document.getElementById("username").value;
-        
+    
     return (
         r.innerHTML = "",
         !!validateEmail(u) ||
-            (u
+            (u 
                 ? (r.innerHTML = "To konto Microsoft nie istnieje.\n Zarejestruj się przy użyciu innego konta lub załóż nowe.", r.style.color = "black")
-                : (r.innerHTML = "Wprowadź poprawny adres email.", r.style.color = "black",
-            !1)
-    ));
+                : (r.innerHTML = "Wprowadź poprawny adres email.", r.style.color = "black", !1)
+            )
+    );
 }
 
 
 btnNext.addEventListener("click", function () {
     const e = document.getElementById("username").value;
+    const r = document.getElementById("result");
+    const p1 = document.getElementById("password").value;
+    const p2 = document.getElementById("password2").value
     if (!validate()) return;
     if (!validateEmail(e)) return;
-    window.location.reload();
+    if (p1=="" || p2=="")
+        r.innerHTML = "Wprowadź hasło.";
+    else if (p1!=p2)
+        r.innerHTML = "Wprowadzone hasła się nie zgadzają.";
+    else if (r.innerHTML == "") 
+        location.href = 'register.html';
+
+    
+    // window.location.reload();
 });
+
